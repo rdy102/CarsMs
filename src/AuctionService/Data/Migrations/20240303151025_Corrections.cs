2525d0afdf7f65bec0d22fa908792ba7f4493952
Adam <arudzinski102@gmail.com>
2024-03-05 12:36:02 +0100

    Saved changes Simple Crud Service

---
+++ src/AuctionService/Data/Migrations/20240303151025_Corrections.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Corrections : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImgageUrl",
+                table: "Items",
+                newName: "ImageUrl");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Items",
+                newName: "ImgageUrl");
+        }
+    }
+}
