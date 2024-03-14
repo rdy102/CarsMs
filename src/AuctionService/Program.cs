13c4649ac0fb688b5f9362f2561dfb1e6dbff4ff
Adam <arudzinski102@gmail.com>
2024-03-14 23:59:48 +0100

    Identity server implemented

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -2,6 +2,7 @@ using AuctionService.Consumers;
 using AuctionService.Data;
 using AuctionService.Entities;
 using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -32,12 +33,20 @@ builder.Services.AddMassTransit(x =>
         });
     }
 );
-
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(opt => 
+{
+    opt.Authority = builder.Configuration["IdentityServiceUrl"];
+    opt.RequireHttpsMetadata = false; 
+    opt.TokenValidationParameters.ValidateAudience = false;
+    opt.TokenValidationParameters.NameClaimType = "username";
+});
 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseAuthentication();
 
 app.UseAuthorization();
 
