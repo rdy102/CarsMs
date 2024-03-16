995c25818073c8bbd19cf16a7500e95cb6049cd3
Adam <arudzinski102@gmail.com>
2024-03-16 23:29:37 +0100

    Gateaway added, front-end client added to Identty service

---
+++ src/SearchService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,24 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService;
+
+public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
+{
+    public async Task Consume(ConsumeContext<AuctionFinished> context)
+    {
+        var acution = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+
+        if(context.Message.ItemSold)
+        {
+            acution.Winner = context.Message.Winner;
+            acution.SoldAmount = (int)context.Message.Amount;
+        }
+
+        acution.Status = "Finished";
+
+        await acution.SaveAsync();
+    }
+}
